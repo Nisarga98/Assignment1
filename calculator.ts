b92d928247f9d36279ea95d42cc18acc939ef479
nisarga <nisargac@maveric-systems.com>
2023-06-07 17:22:51 +0530

    div changes

--- calculator.ts
+++ calculator.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function add() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
@@ -8,10 +7,9 @@ function subtract() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) - Number(num2);
-=======
+}
 function divide() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) / Number(num2);
->>>>>>> a17361775644d1287722ab69379b8e317f8417f8
 }
