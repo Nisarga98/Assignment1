636fd5ed462d5534350fb3f6dc7896aecf472fee
nisarga <nisargac@maveric-systems.com>
2023-06-07 16:31:29 +0530

    merging a tree changes

--- calculator.ts
+++ calculator.ts
@@ -1,12 +1,10 @@
-<<<<<<< HEAD
 function add() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) + Number(num2);
-=======
+}
 function subtract() {
     var num1 = document.getElementById("firstNumber").value;
     var num2 = document.getElementById("secondNumber").value;
     document.getElementById("result").value = Number(num1) - Number(num2);
->>>>>>> 80a28ba4d28b0d227f96b8817d255f2ee3fbbf17
 }
